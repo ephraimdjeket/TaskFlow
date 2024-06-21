ed4e961f3eb441aab8079c6835fabcfe16fefd25
Ephraim Djeket <djeketefra10@hotmail.com>
2024-06-21 15:55:51 +0200

    Added more code and small changes

--- darklight/darklight.js
+++ darklight/darklight.js
@@ -1,17 +1,42 @@
+import {btnDarkToLight, btnLightToDark} from "../handlers/handlers.js"
+
 const moon = document.querySelector(".moon");
 const sun = document.querySelector(".sun");
 const h1 = document.querySelector("h1");
-const p = document.querySelector("p");
+const h2 = document.querySelector("h2");
+const h3 = document.querySelectorAll("h3");
+const li = document.querySelectorAll("li");
 
-export const switcher = () => moon.addEventListener("click", function() {
-    darkToLight();
-})
+export const switcher = () => moon.addEventListener("click", darkToLight);
 
-function darkToLight() {
+export function darkToLight() {
     moon.classList.toggle("hidden")
     sun.classList.toggle("hidden")
     document.body.style.backgroundColor = "#656565";
     h1.classList.toggle("lightmode");
-    p.classList.toggle("lightmode");
-    
+    h2.classList.toggle("lightmode");
+    h3.forEach(item => {
+        item.classList.toggle("lightmode")
+    });
+    const listItems = document.querySelectorAll(".list li");
+    listItems.forEach(item => {
+      item.classList.toggle("lightmode");
+    });
+    btnDarkToLight();
 }
+
+sun.addEventListener("click", function() {
+    moon.classList.toggle("hidden")
+    sun.classList.toggle("hidden")
+    document.body.style.backgroundColor = "";
+    h1.classList.toggle("lightmode");
+    h2.classList.toggle("lightmode");
+    h3.forEach(item => {
+        item.classList.toggle("lightmode")
+    });
+    const listItems = document.querySelectorAll(".list li");
+    listItems.forEach(item => {
+      item.classList.toggle("lightmode");
+    });
+    btnLightToDark();
+})
--- handlers/handlers.js
+++ handlers/handlers.js
@@ -8,8 +8,10 @@ import {
   inputTask,
   addToListBtn,
   taskList,
-  error, removeSingleItem
+  error, removeSingleItem, li
 } from "../index.js";
+
+import { darkToLight } from "../darklight/darklight.js";
 
 const currentMonth = [
   "January",
@@ -51,24 +53,60 @@ export const handleAddToListClick = () => {
   addToListBtn.addEventListener("click", function (e) {
     e.preventDefault();
 
-    if (!errorDisplayed) { // Check if error message is not already displayed
+    if (!errorDisplayed) {
       if (inputTask.value.trim() === "") {
         const errorText = document.createElement("p");
         errorText.textContent = "Add a task to the list.";
         error.style.display = "block";
         error.appendChild(errorText);
-        errorDisplayed = true; 
+        errorDisplayed = true;
 
         setTimeout(() => {
           errorText.remove();
           error.style.display = "none";
-          errorDisplayed = false; 
+          errorDisplayed = false;
         }, 2000);
       } else {
+        const sanitizedTask = DOMPurify.sanitize(inputTask.value);
         const li = document.createElement("li");
-        li.innerHTML = `${inputTask.value} ${removeSingleItem}`;
+        li.innerHTML = `${sanitizedTask} ${removeSingleItem}`;
         taskList.append(li);
+        if (document.body.classList.contains("darkmode")) {
+          li.style.color = "#656565";
+        } else {
+          li.style.color = "dark";
+        }
+        li.addEventListener("click", function () {
+          li.style.textDecoration = li.style.textDecoration === "line-through" ? "none" : "line-through";
+        });
+
+    const removeBtn = li.querySelector('.w-6.h-6.x');
+        removeBtn.addEventListener("click", function (e) {
+          li.remove();
+      })
       }
     }
   });
 };
+
+export function btnDarkToLight() {
+  addToListBtn.style.backgroundColor = "white";
+  addToListBtn.style.color = "#464646";
+  addToListBtn.addEventListener("mouseenter", function() {
+      addToListBtn.style.backgroundColor = "#dcdcdc";
+    });
+    addToListBtn.addEventListener("mouseleave", function() {
+      addToListBtn.style.backgroundColor = "white";
+    });
+}
+
+export function btnLightToDark() {
+  addToListBtn.style.backgroundColor = "#3d3d3d";
+  addToListBtn.style.color = "";
+  addToListBtn.addEventListener("mouseenter", function() {
+      addToListBtn.style.backgroundColor = "#656565";
+    });
+    addToListBtn.addEventListener("mouseleave", function() {
+      addToListBtn.style.backgroundColor = "#3d3d3d";
+    });
+}
